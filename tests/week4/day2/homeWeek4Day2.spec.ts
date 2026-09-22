a594a59ebc2cc0fae92e94f21cd52da6b3c869d1
RAJA-NAGU <nagunglit@gmail.com>
2026-09-22 22:14:08 +0530

    home Assesement week4 day2

--- tests/week4/day2/homeWeek4Day2.spec.ts
+++ tests/week4/day2/homeWeek4Day2.spec.ts
@@ -1,14 +1,19 @@
 import { test, expect } from "@playwright/test";
 
 test("home assesment", async ({ page }) => {
-    await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm")
+
+    await page.goto("https://www.w3schools.com/js/tryit.asp?filename=tryjs_confirm")//open website
+    //register eventlistener before action
     page.on('dialog', async (alert) => {
         console.log("alert message: ", alert.message())
         await alert.accept()
         // await alert.dismiss()
     })
+    //from iframe click button
     await page.frameLocator('#iframeResult').getByRole('button', { name: "Try it" }).click()
+    //assert botton visible
     await expect(page.frameLocator('#iframeResult').getByRole('button', { name: "Try it" })).toBeVisible()
+    // print the message after click
     console.log(await page.frameLocator('#iframeResult').locator('#demo').innerText())
 
 })
